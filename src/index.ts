f4b37e0929be5226e4a5c0d9cf4af6c10d717e9f
cityuserGH <cityuser.public@gmail.com>
2024-08-20 18:20:13 +0200

    Toggle detailed information

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,37 @@ import { landingFormInfo, takeoffFormInfo } from "./data/questions";
 
 createForm("takeoff", "to-form", takeoffFormInfo);
 createForm("landing", "ldg-form", landingFormInfo);
+
+// Show/hide detailed information
+
+const takeoffDetails = document.getElementById(
+    "to-details-table"
+) as HTMLFormElement;
+const landingDetails = document.getElementById(
+    "ldg-details-table"
+) as HTMLFormElement;
+
+const takeoffDetailsButton = document.getElementById(
+    "to-details-button"
+) as HTMLButtonElement;
+const landingDetailsButton = document.getElementById(
+    "ldg-details-button"
+) as HTMLButtonElement;
+
+takeoffDetailsButton.addEventListener("click", () => {
+    takeoffDetails.hidden = !takeoffDetails.hidden;
+    if (takeoffDetails.hidden) {
+        takeoffDetailsButton.textContent = "Show detailed information";
+    } else {
+        takeoffDetailsButton.textContent = "Hide detailed information";
+    }
+});
+
+landingDetailsButton.addEventListener("click", () => {
+    landingDetails.hidden = !landingDetails.hidden;
+    if (landingDetails.hidden) {
+        landingDetailsButton.textContent = "Show detailed information";
+    } else {
+        landingDetailsButton.textContent = "Hide detailed information";
+    }
+});
